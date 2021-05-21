06908a90bf94273f92d6f117f0fb7bc79f26ec5c
Jasmine Lane-Garcia <jasminerlanegarcia@gmail.com>
2021-05-21 10:06:26 -0800

    CodeGrade Set-up

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,8 @@ const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
+  console.log("set up");
+
   useEffect(()=>{
     // make a post request to retrieve a token from the api
     // when you have handled the token, navigate to the BubblePage route
